cdaf23a0b36dc8ed683b88a7e6b287aa35a76e4d
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-13 01:22:34 -0700

    Add user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,29 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < Api::ApplicationController
+    def create
+        user = User.new user_params
+        byebug
+        if user.save
+            render json: {status: 201},status: 201
+        else
+            render json: {status: 400},status: 400
+        end
+    end
+
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+
+    def update_password
+        
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, :email, :phone_number, :role, address: [:street_address, :city, :province, :postal_code])
+    end
+    
 end
