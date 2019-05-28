e218899ca3175758ecd75161077b56e3144643f1
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-28 11:54:48 -0700

    Add validation for sending message after order_item is created in the database

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,5 +6,13 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   
+  after_create :send_message
+
+  private
+  def send_message
+    message = "Your order #{self.order.id} was placed!"
+    number = ENV['TEST_PHONE_NUMBER']
+    self.send_sms(number, message)
+  end
 
 end
