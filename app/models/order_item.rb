76fd4ea6758470573c2cd4871e6e147c1fd48375
Amirsalar Heydari <amirsalaar@gmail.com>
2019-06-03 22:53:36 -0700

    Implement calculating subtotal of discounted items in order_item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,23 +1,17 @@
 class OrderItem < ApplicationRecord
-  
-  # include Messenger
 
   belongs_to :food
   belongs_to :order
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   before_validation :set_subtotal!
 
-  # after_create :send_message
-
   private
-  # def send_message
-  #   message = "Your order #{self.order.id} was placed!"
-  #   number = ENV['TEST_PHONE_NUMBER']
-  #   self.send_sms(number, message)
-  # end
-
   def set_subtotal!
-    self.subtotal = self.quantity * self.food.price
+    if self.food.sale_price != 0
+      self.subtotal = self.quantity * self.food.sale_price
+    else
+      self.subtotal = self.quantity * self.food.price
+    end
   end
 
 end
