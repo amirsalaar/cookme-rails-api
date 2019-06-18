247ba4dce2b4677c343245b7be9eaa4c06dcbaec
Amirsalar Heydari <amirsalaar@gmail.com>
2019-06-18 14:03:54 -0700

    Add host to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,12 @@
+Rails.application.routes.default_url_options = { host: "http://cookme-app-api.herokuapp.com" }
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
+  # Eager load code on boot. This eager loads most of Rai ls and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
@@ -27,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
