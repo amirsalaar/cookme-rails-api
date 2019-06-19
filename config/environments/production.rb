d04ee54d8c6b36696ec048b9cb42c968c98ab2a4
Amirsalar Heydari <amirsalaar@gmail.com>
2019-06-18 18:52:48 -0700

    Change the host to secure HTTPS version of the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.default_url_options = { host: "http://cookme-app-api.herokuapp.com" }
+Rails.application.routes.default_url_options = { host: "https://cookme-app-api.herokuapp.com" }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
